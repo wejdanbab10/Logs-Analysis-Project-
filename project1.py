e5d680f40747c394cdc9d82f706d8d20a9a07171
wejdanbab10 <44418540+wejdanbab10@users.noreply.github.com>
2018-11-17 19:40:56 +0300

    Create project1.py

---
+++ project1.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+
+DBNAME = 'news'
+
+# def get_top_3_popular_articles():
+
+db = psycopg2.connect(database=DBNAME)
+c = db.cursor()
+
+query = \
+    """
+SELECT title,
+       Count(*) AS Views
+FROM   articles
+       INNER JOIN log
+               ON path LIKE Concat('%%', slug)
+GROUP  BY title
+ORDER  BY views DESC
+LIMIT  3;
+"""
+c.execute(query)
+rows = c.fetchall()
+
+print ''
+print 'The most popular three articles of all time:'
+for row in rows:
+    print '  ', u'\u2022 ' + row[0] + ' -- ' + str(row[1]) + ' views'
+
+query = \
+    """
+
+SELECT name,
+       VIEWS
+FROM   authors
+       inner join (SELECT author,
+                          Count(slug) AS VIEWS
+                   FROM   articles
+                          inner join log
+                                  ON path LIKE Concat('%%', slug)
+                   GROUP  BY author)VIEWS
+               ON authors.id = author;
+"""
+c.execute(query)
+rows = c.fetchall()
+
+print ''
+print 'The most popular article authors of all time:'
+for row in rows:
+    print '  ', u'\u2022 ' + row[0] + ' -- ' + str(row[1]) + ' views'
+
+query = \
+    """
+SELECT TEST.DATE, TEST.result
+FROM   (SELECT ( Cast(( Cast (failed_requests.errors AS FLOAT) / Cast(
+                                         all_requests.requests AS FLOAT)
+                      ) AS DECIMAL(16, 4)) * 100 ) AS result,
+               failed_requests.date
+        FROM   (SELECT Date(time),
+                       Count(*) AS errors
+                FROM   log
+                WHERE  status = '404 NOT FOUND'
+                GROUP  BY date) failed_requests
+               JOIN (SELECT Date(time),
+                            Count(*) AS requests
+                     FROM   log
+                     GROUP  BY date) all_requests
+                 ON failed_requests.date = all_requests.date) AS TEST
+WHERE  TEST.result > 1;
+"""
+
+c.execute(query)
+rows = c.fetchall()
+
+print ''
+print 'Days where more than 1% of requests lead to errors:'
+for row in rows:
+    print '  ', u'\u2022 ' + str(row[0]) + ' -- ' + str(row[1]) \
+        + ' % errors'
+print ''
+
+db.close()
+
